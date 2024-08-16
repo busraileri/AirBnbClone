5542cc35c5aa19d75607a6068ae04cd33a5f089a
busraileri <busrailerii@gmail.com>
2024-08-17 00:07:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AirBnb Clone Project
+
+<img src="./public/images/clone-img.JPG" alt="WebPage">
+
+
+[Website](https://busraileri-air-bnb-clone.netlify.app)
+
+
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img className ="hero--photo"
+            src="images/photo-grid.png" 
+            alt="hero"/>
+            
+            <h2 className="hero--header">Online Experiences</h2>
+            
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+
+        </section>
+        
+    )
+}
